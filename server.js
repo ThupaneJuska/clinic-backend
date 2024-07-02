d225e5442a5af91e9bd5fa8cb851375d9179febd
Edward <edwardthupane80@gmail.com>
2024-07-02 10:10:16 +0200

    backend touches

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 const cors = require('cors')
+
+const allowedOrigins = [
+    'http://localhost:4200',
+    'https://clinic-backend-f31ad.web.app'
+  ];
+
 const corsOptions = {
-    origin: 'https://clinic-backend-f31ad.web.app/',
-    optionsSuccessStatus: 200 
-}
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    optionsSuccessStatus: 200
+  };
 
 const mongoose = require('mongoose')
 
